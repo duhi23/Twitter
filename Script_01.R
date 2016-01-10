9b79015865e9bfdff44743f2b20c6d49c9edd777
duhi23 <diego.21.phs@hotmail.com>
2016-01-10 12:30:04 -0500

    Script 01

---
+++ Script_01.R
@@ -0,0 +1,26 @@
+#################################################
+#####     Primera apliacion con TwitteR     #####
+#################################################
+
+install.packages('twitteR', dependencies = TRUE)
+install.packages('base64enc', dependencies = TRUE)
+library(twitteR)
+library(base64enc)
+
+consumer_key <- "gKXs8cHpEqBbBpHY3rotdVTzH"
+consumer_secret <- "tdIGk14MX3ix92u6JiFlUncw6z2q7n1kSbvqsQMEFOxloXfPvv"
+access_token <- "180857891-595URwNzy5nyiCoOsoRvEQWLsghmwm6BJdcQlnDH"
+access_secret <- "nf8EmxldgDH19xLq32ktnt6uWHYjH60dDEBogwig4b9dX"
+
+setup_twitter_oauth(consumer_key, consumer_secret, access_token=access_token, access_secret=access_secret)
+
+# Extrayendo primeros tweets
+
+rstats <- searchTwitter("#rstats", n=9999, since='2015-11-01')
+datos <- do.call("rbind", lapply(rstats, as.data.frame))
+names(datos)
+
+# Tweets de duhi23
+
+duhi <- userTimeline('duhi23', n=100)
+duhi2 <- do.call("rbind", lapply(duhi, as.data.frame))
