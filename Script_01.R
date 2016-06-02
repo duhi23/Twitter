6722c8fa4ff633b8588bc4339453405b49d60c4a
duhi23 <diego.21.phs@hotmail.com>
2016-06-01 21:57:26 -0500

    update

--- Script_01.R
+++ Script_01.R
@@ -18,7 +18,7 @@ setup_twitter_oauth(consumer_key, consumer_secret, access_token=access_token, ac
 
 # Extrayendo primeros tweets
 
-rstats <- searchTwitter("#Pedernales", n=100000, since='2016-04-16')
+rstats <- searchTwitter("#Ecuador", n=20000, since='2016-01-01')
 datos <- do.call("rbind", lapply(rstats, as.data.frame))
 head(datos)
 names(datos)
